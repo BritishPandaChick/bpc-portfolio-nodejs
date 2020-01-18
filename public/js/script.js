11531ac4df68678a1c07bc9a52c2d369ed6c21aa
BritishPandaChick <sbartley1080@gmail.com>
2020-01-18 18:01:51 -0500

    Add code files

---
+++ public/js/script.js
@@ -0,0 +1,76 @@
+//About Page
+//BritishPandaChick Code blog button opens blog when clicked
+function openBritishCodes() {
+  window.open("http://britishpandachickcodes.blogspot.com", "_blank");
+}
+
+//Needlework Kits blog button opens blog when clicked
+function openNeedleKits() {
+  window.open("http://needleworkkits.blogspot.com", "_blank");
+}
+
+//Disney Codes Challenge button opens article
+function openDisneyCodes(){
+  window.open("https://dev.to/theoriginalbpc/everything-you-need-to-know-about-disney-codes-challenge-3ojb");
+}
+
+//Elpha button opens to Elpha home page
+function openElpha(){
+  window.open("https://elpha.com/");
+}
+
+//Portfolio Page
+//Opens Github rep for librarian website
+function openLibrarianSite(){
+  window.open("https://github.com/BritishPandaChick/librarian-portfolio-site");
+}
+
+//Opens Github repo for author website
+function openAuthorSite(){
+  window.open("https://github.com/BritishPandaChick/sean-author-website");
+}
+
+//Open pen for Tribute Page
+function openTributePage(){
+  window.open("https://codepen.io/BritishPandaChick/full/BKBLOv");
+}
+
+//Open pen for Quote Generator project
+function openQuoteGenerate(){
+  window.open("https://codepen.io/BritishPandaChick/full/oLzoBG");
+}
+
+//Open pen for Markdown Previewer project
+function openMarkdownPreview(){
+  window.open("https://codepen.io/BritishPandaChick/full/YoVaZm");
+}
+
+//Open Github repo for Magic 8 Ball project
+function openMagicBall(){
+  window.open("https://github.com/BritishPandaChick/magic-eight-ball");
+}
+
+//Open Github repo for election map project
+function openElectionMap(){
+  window.open("https://github.com/BritishPandaChick/election-map");
+}
+
+//Open Github repo for LOL Cat Clock Project
+function openCatClock(){
+  window.open("https://github.com/BritishPandaChick/lolcatclock");
+}
+
+//Open pen for event invitation project
+function openEventInvite(){
+  window.open("https://codepen.io/BritishPandaChick/full/eEPQza");
+}
+
+//Open pen for movie trailer project
+function openMovieTrailer(){
+  window.open("https://codepen.io/BritishPandaChick/full/pWVjgK");
+}
+
+//Open pen for tourist attraction project
+function openTouristAttract(){
+  window.open("https://codepen.io/BritishPandaChick/full/wqNGKL");
+}
